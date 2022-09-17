7834120367dded17637f8ad095d31975de5e9fe5
Jakob Leifhelm <jakob.leifhelm@gmail.com>
2022-09-17 14:48:43 +0200

    Add documentation

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # A HDLC parsing library
+
 Currently only frame format type 3 is supported. This is the format used in DLMS.
+
+## Documentation
+
+The documentation of the latest version can be found [here](https://docs.rs/hdlcparse/latest/hdlcparse)
--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,27 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
+//! This module can be used to parse HDLC frames.
+//! # Example
+//! ```
+//! use hdlcparse::type3::{HdlcAddress, HdlcFrame};
+//!
+//! let frame: [u8; 33] = [
+//!    0x7E, 0xA0, 0x20, 0x76, 0x54, 0xAE, 0x1B, 0x46, 0xA9, 0x13, 0x2F, 0x2F, /*HCS*/
+//!    0xE6, 0xE6, 0x00, 0x00, 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77, 0x88, 0x99, 0xAA,
+//!    0xBB, 0xCC, 0xDD, 0xEE, 0xFF, 0x4E, 0x66, /*FCS*/
+//! ];
+//! // parse the frame
+//! let hdlc_frame = HdlcFrame::parse(&frame).unwrap();
+//! // check the destination address
+//! assert_eq!(
+//!     hdlc_frame.1.dest_addr,
+//!     HdlcAddress {
+//!         upper: 0x1DAA,
+//!         lower: Some(0x2B8D),
+//!     }
+//! );
+//! ```
 #![cfg_attr(not(feature = "std"), no_std)]
 #![deny(missing_debug_implementations)]
 
@@ -14,12 +35,18 @@ pub mod type3;
 
 const FLAG: u8 = 0x7E;
 
+/// Error used to indicate failed `parse` attempt
 #[derive(Debug, PartialEq, Eq)]
 pub enum Error {
+    /// Indicates that the start character `0x7E` was not the first character
     InvalidStartCharacter,
+    /// This Error is used when the header is not fully present or the length is wrong
     InvalidFormat,
+    /// Used to indicate an invalid address
     InvalidAddress,
+    /// Used when the checksum of the frame is not as expected
     InvalidChecksum,
+    /// Used to indicate that more bytes are needed to fully parse the frame
     Incomplete(Option<NonZeroUsize>),
 }
 
